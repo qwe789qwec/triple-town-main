4abf2c152217cb57240b4a324ee1c19a8a983f32
qwe789qwec <qwe789qwec@gmail.com>
2022-06-26 18:13:27 +0900

    movedata and edit template matching

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # triple-town
+
+
+
+Grass occurred 4193 times (60.505%)
+Bush occurred 1070 times (15.440%)
+Bear occurred 1044 times (15.065%)
+Bot occurred 176 times (2.540%)
+Crystal occurred 168 times (2.424%)
+Tree occurred 136 times (1.962%)
+Ninja occurred 99 times (1.429%)
+Hut occurred 44 times (0.635%)
--- opencvTemplateMatching.py
+++ opencvTemplateMatching.py
@@ -1,17 +1,31 @@
 import numpy as np
 import cv2
 
-img1 = cv2.imread('./temp/dice51.jpg',0)
-img2 = cv2.imread('./temp/dice44.jpg',0)
+# items = ['empty', 'grass', 'bush', 'tree', 'Hut', 'House', 'Mansion',
+#  'Castle', 'floating mansion', 'triple castle', 'Bear', 'Church', 'Cathedral',
+#  'treasure chest', 'big treasure chest', 'bot', 'rock', 'big rock', 'empty',
+#  'triple castle']
+items = ['grass', 'bush', 'tree']
 
 # methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
 #             cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 # methods = [cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF_NORMED]
 methods = [cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF_NORMED]
+number = 1
 
-for method in methods:
-    imgcompare = img1.copy()
-    result = cv2.matchTemplate(imgcompare, img2, method)
-    print(result)
+for item in items:
+    for number in range(1,14):
+        img1 = cv2.imread('./item/' + item + '/' + item + '00.jpg',0)
+        img2 = cv2.imread('./item/' + item + '/' + item + f"{number:02d}" + '.jpg',0)
+        for method in methods:
+            imgcompare = img1.copy()
+            result = cv2.matchTemplate(imgcompare, img2, method)
+            if (method == cv2.TM_CCOEFF_NORMED):
+                print('TM_CCOEFF_NORMED')
+            elif (method == cv2.TM_CCORR_NORMED):
+                print('TM_CCORR_NORMED')
+            elif (method == cv2.TM_SQDIFF_NORMED):
+                print('TM_SQDIFF_NORMED')
+            print(result)
 
 print(type(result[0][0]))
